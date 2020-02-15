da9f393b3bf4feba6c5011a38b7a4467e310c521
ryancollie <58234699+ryancollie@users.noreply.github.com>
2020-02-15 12:47:52 -0400

    Add files via upload

--- budget.sql
+++ budget.sql
@@ -1,1 +1,14 @@
-drop
+create table login(username varchar(50) not null, password varchar(50) not null,
+                   primary key(username));
+
+create table users(username varchar(50) not null, first_name varchar(50) not null, 
+                   last_name varchar(50) not null, primary key(username),
+                   foreign key(username) references login(username));
+
+create table income(username varchar(50) not null, name varchar(50) not null, 
+                    value decimal(10, 2) not null, primary key(username),
+                    foreign key(username) references login(username));
+
+create table expense(username varchar(50) not null, name varchar(50) not null, 
+                     value decimal(10, 2) not null, primary key(username),
+                     foreign key(username) references login(username));
